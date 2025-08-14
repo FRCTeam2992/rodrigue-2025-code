d1b7da13baafcc09fa7adf10861774f4359672b8
Awsome_Creeper9 <gherdt@outlook.com>
2025-08-14 18:38:25 -0500

    Removed IntakeDeploy Subsystem, commands, and constants

--- src/main/java/frc/robot/constants/DebugConstants.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.constants;
-
-public class DebugConstants {
-    public static final boolean dataLogging = false;
-}
--- src/main/java/frc/robot/constants/RobotConstants.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.constants;
-
-public class RobotConstants {
-    public static final double robotPeriod = 0.020;     // 20ms cycles
-
-    // Single vs Double Controller mode
-    // True = single controller mode
-    public static final boolean isSingleController = true;
-}
