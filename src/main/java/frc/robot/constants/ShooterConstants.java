5198b0530be0d9dfde4949e3a43eed7a9241ef13
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2025-08-28 20:22:50 -0500

    Refactor shooter motor configuration and constants naming

--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -3,16 +3,21 @@ package frc.robot.constants;
 public class ShooterConstants {
     public static final double defaultMainShooterSpeed = 2700;
     public static final double defaultSecondaryShooterSpeed = 3300;
-    public static final int shooterEncoderPulses = 2048;
+    public static final double shooterMainRPMTolerance = 200.0;
+    public static final double shooterSecondaryRPMTolerance = 150.0;
     public static final double shooterSpeedTolerance = 100;
     
-    public static final double shooterPIDMainP = 0.1;
-    public static final double shooterPIDMainI = 0.0;
-    public static final double shooterPIDMainD = 1.5;
-    public static final double shooterPIDMainF = 0.055;
+    public static class PIDMain {
+        public static final double kP = 0.1;
+        public static final double kI = 0.0;
+        public static final double kD = 1.5;
+        public static final double kV = 0.055;
+    }
 
-    public static final double shooterPIDSecondaryP = 0.1;
-    public static final double shooterPIDSecondaryI = 0.0;
-    public static final double shooterPIDSecondaryD = 1.5;
-    public static final double shooterPIDSecondaryF = 0.052;
+    public static class PIDSecondary {
+        public static final double kP = 0.1;
+        public static final double kI = 0.0;
+        public static final double kD = 1.5;
+        public static final double kV = 0.052;
+    }
 }
