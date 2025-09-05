66842280d1a1ab2c3dc8fcc41294b250e8ead655
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2025-09-04 20:02:11 -0500

    Add shooter subsystem and test control surface to RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,15 +8,19 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Turret;
+import frc.robot.subsystems.Shooter.ShooterMode;
 
 public class RobotContainer {
   public final Turret turret;
+  public final Shooter shooter;
 
   public final CommandXboxController controllerA;
 
   public RobotContainer() {
     turret = new Turret();
+    shooter = new Shooter();
 
     controllerA = new CommandXboxController(0);
 
@@ -29,6 +33,27 @@ public class RobotContainer {
     controllerA.a().onFalse(new InstantCommand(() -> turret.stopTurret()));
     controllerA.b().whileTrue(new InstantCommand(() -> turret.setTurretSpeed(-0.15)));
     controllerA.b().onFalse(new InstantCommand(() -> turret.stopTurret()));
+
+    controllerA.leftBumper().onTrue(new InstantCommand(() -> {
+      shooter.setMainShooterPower(0.15);
+      shooter.setMode(ShooterMode.ManualSpin);
+    }));
+    controllerA.leftBumper().onFalse(new InstantCommand(() -> shooter.setMode(ShooterMode.Stopped)));
+    controllerA.rightBumper().onTrue(new InstantCommand(() -> {
+      shooter.setSecondaryShooterPower(0.15);
+      shooter.setMode(ShooterMode.ManualSpin);
+    }));
+    controllerA.rightBumper().onFalse(new InstantCommand(() -> shooter.setMode(ShooterMode.Stopped)));
+    controllerA.x().onTrue(new InstantCommand(() -> {
+      shooter.setMainShooterTargetRPM(2000);
+      shooter.setSecondaryShooterTargetRPM(2500);
+      shooter.setMode(ShooterMode.Shooting);
+    }));
+    controllerA.povUp().onTrue(new InstantCommand(() -> shooter.updateMainShooterTargetRPM(50)));
+    controllerA.povDown().onTrue(new InstantCommand(() -> shooter.updateMainShooterTargetRPM(-50)));
+    controllerA.povRight().onTrue(new InstantCommand(() -> shooter.updateSecondaryShooterTargetRPM(50)));
+    controllerA.povLeft().onTrue(new InstantCommand(() -> shooter.updateSecondaryShooterTargetRPM(-50)));
+    controllerA.y().onTrue(new InstantCommand(() -> shooter.setMode(ShooterMode.Stopped)));
   }
 
   public Command getAutonomousCommand() {
